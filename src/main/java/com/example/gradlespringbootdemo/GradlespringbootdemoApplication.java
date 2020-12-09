c52cad5ce3404b118cfffd3cf766a9e61bea7a9d
noyeongKim <househusband813@gmail.com>
2020-12-09 17:17:42 +0900

    Initial commit

---
+++ src/main/java/com/example/gradlespringbootdemo/GradlespringbootdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradlespringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradlespringbootdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradlespringbootdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gradlespringbootdemo/GradlespringbootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradlespringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradlespringbootdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
